2f63e238508a5e945966de3f0210ea6ce9d8cbfa
John Mortensen <jmortensen@powayusd.com>
2020-10-20 17:49:53 -0700

    First Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import projects #projects definitions are placed in different file
+
+# https://flask.palletsprojects.com/en/1.1.x/api/
+from flask import Flask, render_template
+#create a Flask instance
+app = Flask(__name__)
+
+#connects default URL of server to render home.html
+@app.route('/')
+def home_route():
+    return render_template("home.html", projects=projects.setup())
+
+#connects /hello path of server to render hello.html
+@app.route('/hello/')
+def hello_rooute():
+    return render_template("hello.html", projects=projects.setup())
+
+#connects /flask path of server to render flask.html
+@app.route('/flask/')
+def flask_route():
+    return render_template("flask.html", projects=projects.setup())
+
+if __name__ == "__main__":
+    #runs the application on the repl development server
+    app.run(debug=True)
