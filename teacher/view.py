161393a06992a56be0a59ade36f4047a8483a8a5
John Mortensen <jmortensen@powayusd.com>
2020-11-15 19:18:24 -0800

    Support for Java

--- teacher/view.py
+++ teacher/view.py
@@ -1,9 +1,9 @@
 """View in MVC has responsibility for interacting with user"""
-#render_templates is only job required for view
+# render_templates is only job required for view
 from flask import render_template
-#imports defintions of teacher blueprint from __init__.py
+# imports defintions of teacher blueprint from __init__.py
 from teacher import teacher_bp
-#imports from model each data definition
+# imports from model each data definition
 from .model import setup, runtime, alldata, code, code2, journal, planning, playground, calculatorgui
 
 
@@ -25,36 +25,37 @@ def flask_route():
     return render_template("flask.html", projects=setup())
 
 
-@teacher_bp.route("/project/runtime")
+@teacher_bp.route("/project/runtime/")
 def runtime_route():
     return render_template("task.html", data=runtime())
 
 
-@teacher_bp.route("/project/planning")
+@teacher_bp.route("/project/planning/")
 def planning_route():
     return render_template("task.html", data=planning())
 
 
-@teacher_bp.route("/project/journal")
+@teacher_bp.route("/project/journal/")
 def journal_route():
     return render_template("task.html", data=journal())
 
 
-@teacher_bp.route("/project/playground")
+@teacher_bp.route("/project/playground/")
 def playground_route():
     return render_template("task.html", data=playground())
 
 
-@teacher_bp.route("/project/code")
+@teacher_bp.route("/project/code/")
 def code_route():
     return render_template("task.html", data=code())
 
 
-@teacher_bp.route("/project/code2")
+@teacher_bp.route("/project/code2/")
 def code2_route():
     return render_template("task.html", data=code2())
 
-@teacher_bp.route("/project/calculatorGUI")
+
+@teacher_bp.route("/project/calculatorgui/")
 def calculatorgui_route():
     return render_template("task.html", data=calculatorgui())
 
