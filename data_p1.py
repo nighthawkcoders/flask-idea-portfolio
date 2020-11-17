b8614d1fad09a3ec2615358688af25dcb9fd2ced
nakulnandhakumar <55005060+nakulnandhakumar@users.noreply.github.com>
2020-11-17 08:46:37 -0800

    Robot Mania edit

--- data_p1.py
+++ data_p1.py
@@ -5,9 +5,14 @@ from flask import url_for
 
 
 def setup():
-    project1 = model.Project("Project 1", url_for('teacher_bp.index'), "/static/img/cat3.jpg", "Team Teacher",
-                             ["Talented Ghost 1", "Talented Mummy 2", "Smart Witch 3"],
-                            "This project is kind of funky, becuase I am a computer nerd")
-    projects = [project1]
+    # project1 = model.Project("Project 1", url_for('teacher_bp.index'), "/static/img/cat3.jpg", "Team Teacher",
+                            # ["Talented Ghost 1", "Talented Mummy 2", "Smart Witch 3"],
+                            # "This project is kind of funky, becuase I am a computer nerd")
+    
+    p1_robotmania = model.Project("Robot Mania", url_for('p1_robotmania_bp.index'), "/static/img/p1_robotmania.png", "Robot Mania",
+                             ["Nakul Nandhakumar", "Sanvi Pal", "Sara Beniwal", "Vihan Jayaraman", "Yasaswi Singamneni"],
+                             "The main focus of our project wa to make a bunch of fun minigames. There are some other activities that will show up on the menu but the highlights are battleship, connectfour, and hangman")
+    
+    projects = [p1_robotmania]
     period = model.Period("Period 1", "Java Gear Heads", projects)
     return period
