70416b934d8aa19771fe0682b0b40f8e10863852
valeriemiliteeva <valeriemiliteeva@valeries-air.lan>
2020-11-17 09:15:49 -0800

    examples in p4 and p5

--- data_p4.py
+++ data_p4.py
@@ -5,7 +5,9 @@ from flask import url_for
 
 
 def setup():
-    p4_proj1 = []
-    projects = [p4_proj1]
+    EXAMPLE = model.Project("Project Name", "http://www.google.com", "/static/img/cat1.jpg", "Team Name",
+                             ["Talented Student 1", "Talented Student 2", "Smart Student 3", "Team Member 4",
+                              "Amazing Member 5"], "This is our fabulous project, because we are cool (description)")
+    projects = [EXAMPLE]
     period = model.Period("Period 4", "Some really smart people study apcsp here", projects)
     return period
