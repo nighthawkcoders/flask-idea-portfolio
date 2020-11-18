c27175631faaba2ad3340a4c3f38fa4a0ae6b6f8
Hiếu Võ <54915685+tonyhieu@users.noreply.github.com>
2020-11-18 09:23:39 -0800

    Update model.py

--- p1_mortos/model.py
+++ p1_mortos/model.py
@@ -2,8 +2,8 @@ def runtime():
     greeting = "P1 MortOS"
     name = "Carnival"
     doa = "October 30"
-    job = "Press play"
-    embed = "https://Python-Web-Portfolio-Series.jmort1021.repl.co"
-    youtube = ["https://www.youtube.com/embed/R9XeJoNL2kU","https://www.youtube.com/embed/R9XeJoNL2kU"]
+    job = "Press run to play!"
+    embed = "https://repl.it/@AnthonyVo1/java-portfolio?lite=true&outputonly=1"
+    youtube = ["https://www.youtube.com/embed/WiUjG9fF3zw"]
     info = {"greeting": greeting, "name": name, "doa": doa, "job": job, "embed": embed, "youtube":youtube}
     return info
