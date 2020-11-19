018cdbdce4f7877e96e4cc4ecd2d53a227fe791d
sophieleeajh <57341391+sophieleeajh@users.noreply.github.com>
2020-11-18 21:27:06 -0800

    added P4 fruitycoders

--- data_p4.py
+++ data_p4.py
@@ -18,7 +18,12 @@ def setup():
     p4_hangman = model.Project("Music Website", url_for('p4_hangman_bp.index'), "/static/img/p4hangman.png", "P4 Hangman",
                                ["Charlie Zhu", "Rohan Nallapati", "Rivan Nayak", "Sarah Xie", "Noah Pidding"],
                                "This website includes a portfolio of our projects we worked on this trimester as well as a music section including three different genres of music with multiple examples and descriptions of each.")
+    
+    p4_fruitycoders = model.Project("Photography Website", url_for('p4_fruitycoders_bp.index'), "/static/img/p4_fruitycoders.png", "P4 fruitycoders",
+                               ["Sophie Lee", "Linda Long", "Maggie Killada", "Adam Holbel", "Wenshi Bao"],
+                               "Our website (Fruity Photos) features the history of photography, as well as the works and biographies of several famous photographers, such as Ansel Adams and Annie Leibovitz.")
 
-    projects = [p4_slackbots, p4_hangman]
+
+    projects = [p4_slackbots, p4_hangman, p4_fruitycoders]
     period = model.Period("Period 4", "AP Principles of Computer Science - Python", projects)
     return period
