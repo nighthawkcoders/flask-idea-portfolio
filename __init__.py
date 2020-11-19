886c4010f3d6ca8b76455ef7043aef5cd9715fc7
lolabulkin <54717937+lolabulkin@users.noreply.github.com>
2020-11-18 17:06:54 -0800

    added p4_coderjoes

--- __init__.py
+++ __init__.py
@@ -32,6 +32,9 @@ app.register_blueprint(p4_slackbots_bp, url_prefix='/p4_slackbots')
 from p4_hangman import p4_hangman_bp
 app.register_blueprint(p4_hangman_bp, url_prefix='/p4_hangman')
 
+from p4_coderjoes import p4_coderjoes_bp
+app.register_blueprint(p4_coderjoes_bp, url_prefix='/p4_coderjoes')
+
 # Period 5
 from p5_chessGame import p5_chessGame_bp
 app.register_blueprint(p5_chessGame_bp, url_prefix='/p5_chessGame')
--- data_p4.py
+++ data_p4.py
@@ -18,7 +18,9 @@ def setup():
     p4_hangman = model.Project("Music Website", url_for('p4_hangman_bp.index'), "/static/img/p4hangman.png", "P4 Hangman",
                                ["Charlie Zhu", "Rohan Nallapati", "Rivan Nayak", "Sarah Xie", "Noah Pidding"],
                                "This website includes a portfolio of our projects we worked on this trimester as well as a music section including three different genres of music with multiple examples and descriptions of each.")
-
-    projects = [p4_slackbots, p4_hangman]
+    p4_coderjoes = model.Project("CoderJoes", "http://76.176.49.85:3000", "/static/img/p4_coderjoes.png", "P4 Guessers",
+                                 ["Lola Bulkin", "Brayden Basinger", "Billy Cherres", "Grace Le", "Ryan Moghaddas"],
+                                 "This is a grocery store called CoderJoes! You can find lots of cool recipes and descriptions of food, as well as our group work and portfolio!")
+    projects = [p4_slackbots, p4_hangman, p4_coderjoes]
     period = model.Period("Period 4", "AP Principles of Computer Science - Python", projects)
     return period
