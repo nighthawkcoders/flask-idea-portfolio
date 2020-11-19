f07f67194e48dd8d140ce3d837f3db7837dbee90
Kevvinn <kevinhu229@gmail.com>
2020-11-19 15:25:18 -0800

    add picture for p1_grocerystoresim

--- data_p1.py
+++ data_p1.py
@@ -20,12 +20,12 @@ def setup():
     p1_ilikeyacodeg = model.Project("I Like Ya Code G", url_for('p1_ilikeyacodeg_bp.index'), "/static/img/p1_I-like-ya-code-g.png", "I Like Ya Code G",
                                     ["Dhruv Kanetkar", "Tanay Shah", "Gautam Gupta", "Dhruv Sengupta"], "This is our Calculator Suite that allows the user to calculate a variety of things. "
                                                                                                         "It can do basic scientific calculation, convert units and binary values, sovle derivatives, and solve physics kinematic problems")
-
+    
     p1_mortos = model.Project("MortOS", url_for('p1_mortos_bp.index'), "/static/img/p1-mortos-examplepic.PNG", "MortOS",
                              ["Nathaniel Cherian", "Benjamin Herrick", "Jett Kim", "Brendan Trinh", "Anthony Vo"],
                              "Our project was an exploration of different coding topics and concepts that's displayed through " +
                              "a variety of minigames. Try some of them out!")
-    p1_grocerystoresim = model.Project("Boomer Esports", url_for('p1_grocerystoresim_bp.index'), "/static/img/p1_grocerystore.png", "Grocery Store Sim",
+    p1_grocerystoresim = model.Project("Boomer Esports", url_for('p1_grocerystoresim_bp.index'), "/static/img/p1_grocery.png", "Grocery Store Sim",
                              ["Kevin Hu", "Sean Tran", "Aditya Surapaneni", "Siddhant Ranka", "Jacob Rozenkrants"],
                              "Tired of modern flashy video games and want a blast to the past? " +
                              "Check out some of these classic games that are a nice break from the fast paced and stressful modern games.")
