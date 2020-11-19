f82c82c02218011dbdb1754ac95727cc15045c3f
kira <70781122+4DISEASE@users.noreply.github.com>
2020-11-19 15:36:15 -0800

    Update data_p5.py

--- data_p5.py
+++ data_p5.py
@@ -12,7 +12,11 @@ def setup():
                             ["Karam Alshaikh", "Akhilesh Genneri", "Akshit Prathipati", "Noya Hafiz",
                              "Jien (Max) Wang"], "This website is used for everything calculus and to spread our information about it")
 
+    p5_supercool = model.Project("p5_supercool", "http://76.176.107.1/", "/static/img/p5_supercool.jpg", "Super Cool",
+                            ["James Hunt", "Mackenzie Aboy", "Lucas Kaimer", "Sam Koenig",
+                             "Kira Liao"], "This webpage is all about countries around the world. Look through each of the pages to learn fun facts, see COVID-19 stats, and play our rendition of Geoguessr.")
 
-    projects = [EXAMPLE,p5_calculus]
+
+    projects = [EXAMPLE,p5_calculus,p5_supercool]
     period = model.Period("Period 5", "Some really smart people study apcsp here", projects)
     return period
