56711d58f674e95bd139d477cf697233d82ed21e
sophieleeajh <57341391+sophieleeajh@users.noreply.github.com>
2020-11-18 22:24:41 -0800

    Update view.py

--- p4_fruitycoders/view.py
+++ p4_fruitycoders/view.py
@@ -1,59 +1,73 @@
-from flask import Flask, render_template,redirect,url_for
+from flask import Flask, render_template
 from p4_fruitycoders import p4_fruitycoders_bp
 
 app = Flask(__name__)
+
 
 @p4_fruitycoders_bp.route('/')
 def home_route():
     return render_template("home.html")
 
+
 @p4_fruitycoders_bp.route('/history/')
 def hello_route():
     return render_template("history.html")
+
 
 @p4_fruitycoders_bp.route('/biographies/')
 def biographies_route():
     return render_template("biographies.html")
 
+
 @p4_fruitycoders_bp.route('/aboutus/')
 def aboutus_route():
     return render_template("aboutus.html")
+
 
 @p4_fruitycoders_bp.route('/adams/')
 def adams_route():
     return render_template("adams.html")
 
+
 @p4_fruitycoders_bp.route('/bourke/')
 def bourke_route():
     return render_template("bourke.html")
+
 
 @p4_fruitycoders_bp.route('/goldin/')
 def goldin_route():
     return render_template("goldin.html")
 
+
 @p4_fruitycoders_bp.route('/leibovitz/')
 def leibovitz_route():
     return render_template("leibovitz.html")
+
 
 @p4_fruitycoders_bp.route('/mann/')
 def mann_route():
     return render_template("mann.html")
 
+
 @p4_fruitycoders_bp.route('/mccurry/')
 def mccurry_route():
     return render_template("mccurry.html")
+
 
 @p4_fruitycoders_bp.route('/newman/')
 def newman_route():
     return render_template("newman.html")
 
+
 @p4_fruitycoders_bp.route('/pretty/')
 def pretty_route():
     return render_template("pretty.html")
 
+
 @p4_fruitycoders_bp.route('/shabazz/')
 def shabazz_route():
     return render_template("shabazz.html")
+
 
 @p4_fruitycoders_bp.route('/wong/')
 def wong_route():
