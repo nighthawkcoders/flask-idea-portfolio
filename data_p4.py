f16059e300966672168af9b43e548ca77d4a5e83
kaila <kaila041603@icloud.com>
2020-11-19 16:31:50 -0800

    p4 charlieb was modified :)

--- data_p4.py
+++ data_p4.py
@@ -23,7 +23,7 @@ def setup():
                                ["Sophie Lee", "Linda Long", "Maggie Killada", "Adam Holbel", "Wenshi Bao"],
                                "Our website (Fruity Photos) features the history of photography, as well as the works and biographies of several famous photographers, such as Ansel Adams and Annie Leibovitz.")
 
-    p4_charlieb = model.Project("Gaming Website", "http://104.63.255.27/home", "/static/img/p4_charlieb.png", "P4 CharlieB",
+    p4_charlieb = model.Project("Gaming Website", url_for('p4_charlieb_bp.home'), "/static/img/p4_charlieb.png", "P4 CharlieB",
                                     ["Kaila Manangan", "Eshaan Parlikar", "Brent Arcinue", "Calvin Ni"],
                                     "Our website, along with portfolio information, includes minigames for your entertainment!")
 
--- p4_charlieb/view.py
+++ p4_charlieb/view.py
@@ -6,118 +6,117 @@ app = Flask(__name__)
 
 #home screen
 @p4_charlieb_bp.route("/home")
-@p4_charlieb_bp.route("/")
 def home():
-  return render_template("home.html")
+  return render_template("p4_charlieb/home.html")
 
 #playgrounds
 @p4_charlieb_bp.route("/playgrounds")
 def playgrounds():
-  return render_template("playgrounds.html")
+  return render_template("p4_charlieb/playgrounds.html")
 
 #themes
 @p4_charlieb_bp.route("/themes")
 def themes():
-  return render_template("themes.html")
+  return render_template("p4_charlieb/themes.html")
 
 #github
 @p4_charlieb_bp.route("/github")
 def github():
-  return render_template("github.html")
+  return render_template("p4_charlieb/github.html")
 
 #project plan
 @p4_charlieb_bp.route("/projectplan")
 def projectplan():
-  return render_template("projectplan.html")
+  return render_template("p4_charlieb/projectplan.html")
 
 @p4_charlieb_bp.route("/image")
 def images():
-  return render_template("image.html")
+  return render_template("p4_charlieb/image.html")
 
 @p4_charlieb_bp.route("/rimages")
 def rimages():
-  return render_template("rimages.html")
+  return render_template("p4_charlieb/rimages.html")
 
 @p4_charlieb_bp.route("/trivia")
 def trivia():
-  return render_template("trivia.html")
+  return render_template("p4_charlieb/trivia.html")
 
 @p4_charlieb_bp.route("/rocks")
 def rocks():
-  return render_template("rocks.html")
+  return render_template("p4_charlieb/rocks.html")
 
 @p4_charlieb_bp.route("/randomnum")
 def randomnum():
-  return render_template("randomnum.html")
+  return render_template("p4_charlieb/randomnum.html")
 
 @p4_charlieb_bp.route("/battleship")
 def battleship():
-  return render_template("battleship.html")
+  return render_template("p4_charlieb/battleship.html")
 
 @p4_charlieb_bp.route("/kailavideo")
 def kailavideo():
-  return render_template("kailavideo.html")
+  return render_template("p4_charlieb/kailavideo.html")
 
 @p4_charlieb_bp.route("/calvinvideo")
 def calvinvideo():
-  return render_template("calvinvideo.html")
+  return render_template("p4_charlieb/calvinvideo.html")
 
 @p4_charlieb_bp.route("/kailasgoals")
 def kailasgoals():
-  return render_template("kailasgoals.html")
+  return render_template("p4_charlieb/kailasgoals.html")
 
 @p4_charlieb_bp.route("/eshaangoals")
 def eshaangoals():
-  return render_template("eshaangoals.html")
+  return render_template("p4_charlieb/eshaangoals.html")
 
 @p4_charlieb_bp.route("/brentgoals")
 def brentgoals():
-  return render_template("brentgoals.html")
+  return render_template("p4_charlieb/brentgoals.html")
 
 @p4_charlieb_bp.route("/calvingoals")
 def calvingoals():
-  return render_template("calvingoals.html")
+  return render_template("p4_charlieb/calvingoals.html")
 
 @p4_charlieb_bp.route("/gametest")
 def gametest():
-  return render_template("gametest.html")
+  return render_template("p4_charlieb/gametest.html")
 
 @p4_charlieb_bp.route("/eshaanvideo")
 def eshaanvideo():
-    return render_template("eshaanvideo.html")
+    return render_template("p4_charlieb/eshaanvideo.html")
 
 @p4_charlieb_bp.route("/pong")
 def pong():
-  return render_template("pong.html")
+  return render_template("p4_charlieb/pong.html")
 
 @p4_charlieb_bp.route("/pacman")
 def pacman():
-  return render_template("pacman.html")
+  return render_template("p4_charlieb/pacman.html")
 
 @p4_charlieb_bp.route("/tics")
 def tics():
-  return render_template("tics.html")
+  return render_template("p4_charlieb/tics.html")
 
 @p4_charlieb_bp.route("/snake")
 def snake():
-  return render_template("snake.html")
+  return render_template("p4_charlieb/snake.html")
 
 @p4_charlieb_bp.route("/brentvideo")
 def brentvideo():
-  return render_template("brentvideo.html")
+  return render_template("p4_charlieb/brentvideo.html")
 
 @p4_charlieb_bp.route("/helloseries")
 def helloseries():
-  return render_template("helloseries.html")
+  return render_template("p4_charlieb/helloseries.html")
 
 @p4_charlieb_bp.route("/flask")
 def flask():
-  return render_template("flask.html")
+  return render_template("p4_charlieb/flask.html")
 
 @p4_charlieb_bp.route("/selfgrading")
 def selfgrading():
-  return render_template("selfgrading.html")
+  return render_template("p4_charlieb/selfgrading.html")
 
 @p4_charlieb_bp.route("/journals")
 def journals():
-  return render_template("journals.html")
+  return render_template("p4_charlieb/journals.html")
