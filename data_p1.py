406087072b5fdcd1ecb10c45d9e6a181eb741afa
Neil Sahai <54915644+Neil-Sahai@users.noreply.github.com>
2020-11-19 20:21:40 -0800

    Update data_p1.py

--- data_p1.py
+++ data_p1.py
@@ -32,7 +32,12 @@ def setup():
                               ["Aidan Rosen", "Andrew Hale", "Andrew Joseph", "Alex Hu", "Atharva Kudrimoti"], 
                                "This project is a combination of a password manager, vector calculator and graphing calculator")
     
-    projects = [p1_robotmania, p1_dataminers, p1_ilikeyacodeg, p1_mortos, p1_grocerystoresim, p1_hexpass] #, p1_grocerystoresim
+    p1_asciiimenu = model.Project("CSA Portfolio", url_for('p1_asciimenu_bp.index'), "/static/img/natm.PNG", "CSA Portfolio",
+                                       ["Neil Sahai", "Eyaad Mir", "Andrew Pu", "Alex Titov", "Sean Rollins"],
+                                       "Play some games and get money to buy Calculator Functions" +
+                                       "Try out some of these engaging games and remember to save some money in the ATM!")
+    
+    projects = [p1_robotmania, p1_dataminers, p1_ilikeyacodeg, p1_mortos, p1_grocerystoresim, p1_hexpass, p1_asciimenu] #, p1_grocerystoresim
     period = model.Period("Period 1", "Computer Science A - Java", projects)
 
     return period
