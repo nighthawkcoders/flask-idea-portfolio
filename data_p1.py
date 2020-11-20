2929a8fbe18c468942931923eb04423ab7ffa4f5
nakulnandhakumar <55005060+nakulnandhakumar@users.noreply.github.com>
2020-11-19 20:15:26 -0800

    Update data_p1.py

--- data_p1.py
+++ data_p1.py
@@ -11,8 +11,8 @@ def setup():
     
     p1_robotmania = model.Project("Robot Mania", url_for('p1_robotmania_bp.index'), "/static/img/p1_robotmania.png", "Robot Mania",
                              ["Nakul Nandhakumar", "Sanvi Pal", "Sara Beniwal", "Vihan Jayaraman", "Yasaswi Singamneni"],
-                             "The main focus of our project was to make a bunch of fun minigames. There are some other activities "
-                             "that will show up on the menu but the highlights are battleship, connect-four, and hangman.")
+                             "The main focus of our project was to make a bunch of fun minigames and a great calculator. There are some other activities "
+                             "that we created but this project is mainly to show of our calculator.")
     p1_dataminers = model.Project("Dataminers", url_for('p1_dataminers_bp.index'), "/static/img/p1_dataminersTile.png", "Dataminers",
                                                   ["Andrew Pegg", "David Ramsayer", "Dominic Phung", "Michael Hayes", "Jason Chang"],
                                                   "This project has many different demos in a single program: notepad, unit converter, calculator, tic tac toe, and hangman.")
