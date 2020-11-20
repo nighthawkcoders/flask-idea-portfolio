7d7160c0d73fcd00fedfe24e7aede56699691d7c
sophieleeajh <57341391+sophieleeajh@users.noreply.github.com>
2020-11-19 23:00:33 -0800

    Update data_p4.py

--- data_p4.py
+++ data_p4.py
@@ -24,7 +24,7 @@ def setup():
                                "as a music section including three different genres of music with multiple examples "
                                "and descriptions of each.")
 
-    p4_fruitycoders = model.Project("Photography Website", "google.com", "/static/img/cat2.png",
+    p4_fruitycoders = model.Project("Photography Website", url_for('p4_fruitycoders_bp.index'), "/static/img/cat2.png",
                                     "P4 fruitycoders",
                                     ["Sophie Lee", "Linda Long", "Maggie Killada", "Adam Holbel", "Wenshi Bao"],
                                     "Our website (Fruity Photos) features the history of photography, as well as the "
