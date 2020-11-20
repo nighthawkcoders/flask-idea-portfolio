9f3d0e1aaf7a09f8222226f0b04cd76d98fde5c9
Abhijay <fornitefrogs@gmail.com>
2020-11-19 21:36:09 -0800

    slashes

--- p4_slackbots/view.py
+++ p4_slackbots/view.py
@@ -15,58 +15,58 @@ def startprogram():
 #@p4_slackbots_bp.route("/home")
 
 
-@p4_slackbots_bp.route('/slackbots')
+@p4_slackbots_bp.route('/slackbots/')
 def slackbots():
 	return render_template("about.html", groupdatalist=groupdata())
 
-@p4_slackbots_bp.route('/kevin')
+@p4_slackbots_bp.route('/kevin/')
 def kevin():
 	return render_template("indivabout.html", data=kevin())
 
-@p4_slackbots_bp.route('/abhijay')
+@p4_slackbots_bp.route('/abhijay/')
 def abhijay():
 	return render_template("indivabout.html", data=abhijay())
 
-@p4_slackbots_bp.route('/paul')
+@p4_slackbots_bp.route('/paul/')
 def paul():
 	return render_template("indivabout.htm", data=paul())
 
-@p4_slackbots_bp.route('/travis')
+@p4_slackbots_bp.route('/travis/')
 def travis():
 	return render_template("indivabout.html", data=travis())
 
-@p4_slackbots_bp.route('/gavin')
+@p4_slackbots_bp.route('/gavin/')
 def gavin():
 	return render_template("indivabout.html", data=gavin())
 
-@p4_slackbots_bp.route('/baseview')
+@p4_slackbots_bp.route('/baseview/')
 def baseview():
 	return render_template("base-productview.html")
 
-@p4_slackbots_bp.route('/shoebaseview')
+@p4_slackbots_bp.route('/shoebaseview/')
 def shoebaseview():
 	return render_template("base-productview-shoe.html")
 
-@p4_slackbots_bp.route('/APCOFluid')
+@p4_slackbots_bp.route('/APCOFluid/')
 def APCOFluid():
 	return render_template("apco-fluid.html")
 
-@p4_slackbots_bp.route('/APCOSuperstar')
+@p4_slackbots_bp.route('/APCOSuperstar/')
 def APCOSuperstar():
 	return render_template("apco-superstar.html")
 
-@p4_slackbots_bp.route('/GGFireworks')
+@p4_slackbots_bp.route('/GGFireworks/')
 def GGFireworks():
 	return render_template("gg-fireworks.html")
 
-@p4_slackbots_bp.route('/GGSuperstar')
+@p4_slackbots_bp.route('/GGSuperstar/')
 def GGSuperstar():
 	return render_template("gg-superstar.html")
 
-@p4_slackbots_bp.route('/Products')
+@p4_slackbots_bp.route('/Products/')
 def Products():
 	return render_template("products.html")
 
-@p4_slackbots_bp.route('/Cart')
+@p4_slackbots_bp.route('/Cart/')
 def Cart():
 	return render_template("cart.html")
