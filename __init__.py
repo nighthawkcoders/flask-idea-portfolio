8daa6064ebbd2041fc6b932cf5889cf6e501c725
John Mortensen <jmortensen@powayusd.com>
2020-11-19 19:42:55 -0800

    restoring p2 projects

--- __init__.py
+++ __init__.py
@@ -28,6 +28,9 @@ app.register_blueprint(cyphercrypto_bp, url_prefix='/p2_cyphercrypto')
 from p2_dinos import p2_dinos_bp
 app.register_blueprint(p2_dinos_bp, url_prefix='/p2_dinos')
 
+from p2_triviagame import p2_triviagame_bp
+app.register_blueprint(p2_triviagame_bp, url_prefix='/p2_triviagame')
+
 # Period 4
 from p4_slackbots import p4_slackbots_bp
 app.register_blueprint(p4_slackbots_bp, url_prefix='/p4_slackbots')
